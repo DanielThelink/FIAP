110e465ea34b1a38c2e1b5d5848a05ec61f2a013
DanielTheLink <daniel.f.barros@hotmail.com>
2022-04-01 20:56:38 -0300

    initial commit 1

---
+++ Comp_Think_Python/aula_while.py
@@ -0,0 +1,101 @@
+'''
+x= 0
+
+while x<100:
+    x=x+1
+    print(x)
+'''
+'''
+x= 49
+
+while x < 100:
+
+    x = x + 1
+    print(x)
+'''
+
+'''
+contagem = 11
+
+while contagem >= 1:
+    contagem = contagem - 1
+    print(contagem)
+
+else:
+
+    print("FOGO!")
+'''
+'''
+fim = int(input("Digite o numero:"))
+
+
+x= 0
+
+while x <= fim:
+    x += 1
+    print(x)
+'''
+
+
+'''
+NUMERO IMPAR
+
+num_usuario = int(input("Digite seu numero impar:"))
+
+x = 1
+while num_usuario>x:
+    x = x + 2
+    print(x)
+'''
+'''
+x = 3
+
+while x <= 30:
+    print(x)
+    x+= 3
+'''
+'''
+#Tabuada
+
+num = int(input("Tavuada do:"))
+
+contador = 1
+
+while contador <=10:
+
+    tabuada = num * contador
+
+    print(tabuada)
+
+    contador += 1
+'''
+'''
+# TABUADA BONITA
+
+num = int(input("Tabuada do:"))
+
+contador = 1
+
+while contador <=10:
+
+    tabuada = num * contador
+
+    print(f"{num} X {contador}=",tabuada)
+
+    contador += 1
+'''
+'''
+# USUARIO DIGITA O INICIO E O FIM DA TABUADA
+
+num = int(input("Tabuada do:"))
+
+contador = int(input("inicio de tabuada:"))
+final = int(input("fim:"))
+
+while contador <=final:
+
+    tabuada = num * contador
+    print(f"{num} X {contador}=", tabuada)
+    contador += 1
+'''
+
