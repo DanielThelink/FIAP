d885bbdb54f02bbcce6f8783b2630520b01e748b
logonrmlocal <2wfg2fe2>
2022-04-04 21:19:20 -0300

    commit 3

--- Comp_Think_Python/aula_while.py
+++ Comp_Think_Python/aula_while.py
@@ -98,5 +98,81 @@ while contador <=final:
     print(f"{num} X {contador}=", tabuada)
     contador += 1
 '''
-#fim
+#8
+'''
+deposito_inicial = float(input("Digite o valor do deposito no mes:"))
+taxa_juros = float(input("Taxa de Juros %:"))
+mes = 1
+
+saldo = deposito_inicial
+
+while mes <= 24:
+    saldo = saldo + saldo * (taxa_juros/100)
+    print(f"saldo do mes {mes} é igual a R$ {saldo:.2f}")
+    mes += 1
+print(f"O ganho total dos meses é: {saldo:.2f} ")
+'''
+
+#9 alterando para perguntar o deposito mensal
+'''
+deposito_inicial = float(input("Digite o valor do deposito no mes:"))
+deposito_mensal = float(input("Digite o valor do deposito mensal:"))
+taxa_juros = float(input("Taxa de Juros %:"))
+mes = 1
+
+saldo = deposito_inicial
+
+while mes <= 12:
+    saldo = saldo + (saldo + deposito_mensal) * (taxa_juros/100)
+    print(f"saldo do mes {mes} é igual a R$ {saldo:.2f}")
+    mes += 1
+print(f"O ganho total dos meses é: {saldo:.2f} ")
+'''
+
+# funcao Break
+'''
+soma = 0
+while True:
+    num = int(input("Digite um numero para somar ou 0 para sair:"))
+    if num == 0:
+        break
+    soma += num # apos digitar 0 ele para o codigo e faz a soma da variavel "num"
+print(f"Soma ={soma}")
+'''
+#POSICAO  0 1 2 ...
+'''
+Lista1 = [1,2,3]
+
+print(Lista1) #printa a lista inteira
+
+print(Lista1[2]) #printa o terceiro elemento da lista
+
+Lista1[0] = 10 #muda o valor do primeiro elemento da lista para 10
+
+'''
+'''
+notas = [6,7,5,8,9]
+soma = 0
+x = 0 # 0 porque comeca na posicao 0 da lista que seria a do "6"
+
+while x < 5: # na ultima rodagem ele entra como valor 4
+    soma += notas[x]
+    x += 1 # e aqui ele soma o valor 4 com 1 e ele sai 5
+
+print(f"Media = {(soma/x):.2f}") #aqui ele ja soma o valor 5 de x
+'''
+
+notas = [0,0,0,0,0]
+posicao_lista = ["Primera","Segunda","Terceira","Quarta","Quinta"]
+soma = 0
+x = 0
+
+while x < 5:
+    notas[x] = float(input(f"{posicao_lista[x]} Nota ="))
+    soma = soma + notas[x]
+    x += 1
+
+print(f"Media = {(soma/x):.2f}")
+
+
 
